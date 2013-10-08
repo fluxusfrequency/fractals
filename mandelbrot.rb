0b821aef566e9915e0cd38f8e52e5c07ebc827a8
Ben Lewis <bennlewis@gmail.com>
2013-10-08 13:38:40 -0600

    played with zooming in and out, as well as color animation

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -7,8 +7,8 @@ class MandelbrotFractal < Processing::App
   attr_accessor :x, :y, :xmin, :xmax, :ymin, :ymax, :dx, :dy, :w, :h, :maxiterations, :a, :b, :n, :aa, :bb, :twoab
 
   def setup
-    size(1280, 720)
-    noLoop()
+    size(640, 360)
+    # noLoop()
     background(255)
 
     # Make sure we can write to the pixels[] array.
@@ -19,14 +19,14 @@ class MandelbrotFractal < Processing::App
     # Establish a range of values on the complex plane
     # A different range will allow us to "zoom" in or out on the fractal
     # float xmin = -1.5 float ymin = -.1 float wh = 0.15
-    @xmin = -3.0
-    @ymin = -1.25
-    @w = 5.0
-    @h = 2.5
+    @xmin = -1.8
+    @ymin = -1.7
+    @w = 3.0
+    @h = 3.0
 
 
     # Maximum number of iterations for each point on the complex plane
-    @maxiterations = 100
+    @maxiterations = 1
 
 
     # x goes from xmin to xmax
@@ -38,12 +38,15 @@ class MandelbrotFractal < Processing::App
     # Calculate amount we increment x,y for each pixel
     @dx = (xmax - xmin) / (width)
     @dy = (ymax - ymin) / (height)
+
+    @going_up = false
   end
 
 
 
 
   def draw
+    puts "Drawing"
     # Start y
     @y = ymin
     for j in 0..height-1 do
@@ -75,20 +78,44 @@ class MandelbrotFractal < Processing::App
           pixels[i+j*width] = color(@n*16 % 200, @n*16 % 64, @n*16 % 256)
         else
           # Gosh, we could make fancy colors here if we wanted
-          pixels[i+j*width] = color(@n*16 % 256, @n*16 % 1, @n*16 % 256)
+          pixels[i+j*width] = color(@n*16 % 200, @n*16 % 64, @n*16 % 256)
         end
         @x += @dx
       end
       @y += @dy
     end
     updatePixels()
+    @maxiterations += 2
     # update_range
   end
 
-  # def update_range
-  #   @xmin -= 0.5
-  #   @ymin -= -0.25
-  # end
+  def update_range
+    if @going_up
+      count_up
+      @going_up == false # if @xmin >= -0.5 || @xmin == 0
+    else
+      count_down
+      @going_up == true # if @xmin <= -5.0
+    end
+  end
+
+  def count_up
+    @xmin = @xmin + 0.1
+    @ymin = @ymin + 0.25
+    @w = @w + 0.1
+    @h = @h + 0.1
+    @xmax = xmin + w
+    @ymax = ymin + h
+  end
+
+  def count_down
+    @xmin = @xmin - 0.1
+    @ymin = @ymin - 0.25
+    @w = @w - 0.1
+    @h = @h - 0.1
+    @xmax = xmin + w
+    @ymax = ymin + h
+  end
 
 end
 
